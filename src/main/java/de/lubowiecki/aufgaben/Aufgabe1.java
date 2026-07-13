d44b76e087e9eee4c3f985324d71bd37358e009d
tlubowiecki <tomasz@lubowiecki.de>
2026-07-13 12:49:28 +0200

    Init

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe1.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe1.java
@@ -6,6 +6,7 @@ public class Aufgabe1 {
         Schaue dir die primitiven Datentypen in den KeyNotes an
         und schreibe für jeden Datentyp 2 Variablendeklarationen.
         Beachte bei der Benennung die Namenskonventionen.
+        ...
     */
 
 }
