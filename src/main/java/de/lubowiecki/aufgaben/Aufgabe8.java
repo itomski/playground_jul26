eb51fd8cc3a68ce73bffdd1cc8a4f630f23b2215
tlubowiecki <tomasz@lubowiecki.de>
2026-07-15 14:30:03 +0200

    Methoden, Aufgabe 8 Update

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe8.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe8.java
@@ -11,6 +11,22 @@ public class Aufgabe8 {
 
     Zusätzlich soll es eine Methode geben, die als Parameter eine Anzahl bekommt und ein Array mit
     entsprechender Anzahl Zufallszahlen liefert.
+
+    Erweiterung:
+    Entwerfe eine zweite Version des Würfelbechers mit neuen Features.
+    Der Zufallswert liegt zwischen 1 und max. Augenanzahl.
+
+    Der Würfelbecher kann bei der Instanzierung einen Wert bekommen, der die maximale Anzahl der Augen
+    für die Zufallswürfe definiert.
+
+    Bespiele:
+
+    Würfelbecher cup1 = new Würfelbecher(20);
+    cup1.roll(); // Liefert einen Wert zwischen 1 und 20
+
+    Würfelbecher cup2 = new Würfelbecher(100);
+    cup2.roll(); // Liefert einen Wert zwischen 1 und 100
+
      */
 
 }
