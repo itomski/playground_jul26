0f32f5f00bd411eb2240e93a64d252acdf01bdc1
tlubowiecki <tomasz@lubowiecki.de>
2026-07-30 09:57:35 +0200

    Aufgabe 29 JavaFx

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe29.java
@@ -0,0 +1,10 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe29 {
+
+    /*
+    Erweitere die Shopping-List und zwei weitere Textfelder (Anzahl und Bemerkung)
+    Lese beide Felder beim Speichern aus und ergänze die Ausgabe.
+    */
+
+}
