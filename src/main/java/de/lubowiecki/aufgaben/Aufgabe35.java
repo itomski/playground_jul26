01ae5b0af5b8ae8a15e4e0f3512537601cc9d376
tlubowiecki <tomasz@lubowiecki.de>
2026-08-11 14:42:43 +0200

    Aufgabe 36

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe35.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe35.java
@@ -5,6 +5,6 @@ public class Aufgabe35 {
     /*
     Erweitere das Projekt "Bibliothek" um die möglichkeit Kunden zu verwalten.
     Dabei soll es dafür eine eigene API geben über die ein Speichern, Anzeigen und Löschen von Kunden möglich ist.
-     */
+    */
 
 }
---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe36.java
@@ -0,0 +1,15 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe36 {
+
+    public class Aufgabe35 {
+
+    /*
+    Erweitere das Projekt "Bibliothek" um die möglichkeit Kunden per Web-Interface zu verwalten.
+    Dabei soll es dafür ein passender Controller (Speichern, Anzeigen und Löschen) entwickelt werden.
+    Verwende dazu ein neues HTML-Teplate.
+    */
+
+    }
+
+}
