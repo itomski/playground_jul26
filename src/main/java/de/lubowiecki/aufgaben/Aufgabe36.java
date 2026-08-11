36b5d7c07868975892a7453f6dadda20b3bd6283
tlubowiecki <tomasz@lubowiecki.de>
2026-08-11 14:44:00 +0200

    Aufgabe 36

--- src/main/java/de/lubowiecki/aufgaben/Aufgabe36.java
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe36.java
@@ -2,14 +2,10 @@ package de.lubowiecki.aufgaben;
 
 public class Aufgabe36 {
 
-    public class Aufgabe35 {
-
     /*
     Erweitere das Projekt "Bibliothek" um die möglichkeit Kunden per Web-Interface zu verwalten.
     Dabei soll es dafür ein passender Controller (Speichern, Anzeigen und Löschen) entwickelt werden.
     Verwende dazu ein neues HTML-Teplate.
     */
-
-    }
 
 }
