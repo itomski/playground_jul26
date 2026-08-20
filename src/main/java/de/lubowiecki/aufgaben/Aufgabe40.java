48fca5c833ef955dd440c5d7cb0df0f2c7c8d4b5
tlubowiecki <tomasz@lubowiecki.de>
2026-08-20 13:49:25 +0200

    Aufgabe 40

---
+++ src/main/java/de/lubowiecki/aufgaben/Aufgabe40.java
@@ -0,0 +1,14 @@
+package de.lubowiecki.aufgaben;
+
+public class Aufgabe40 {
+
+    /*
+    Schreibe eine Klasse mit 4 Methoden, die Parameter verarbeiten
+    Methode 1: Ob Text länger als 10 und kürzer als 30 Zeichen ist
+    Methode 2: Prüfen, ob ein bestimmtes Wort im Text vorkommt
+    Methode 3: Eine Sammlung an Zufallszahlen soll eine 5 enthalten
+    Methode 4: Eine Liste soll 5 Strings enthalten
+    Teste die Methoden mit JUnit.
+    */
+
+}
